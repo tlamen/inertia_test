8d97707ad5e27f3df26c1dd76b3c950a176483e4
tlamen <bernardolobo3@gmail.com>
2023-08-30 09:42:35 -0300

    :white_check_mark: first pending test

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    task = Task.new(
+      nome: 'Sample Task',
+      descrição: 'This is a sample task',
+      status: 'pending'
+    )
+    expect(task).to be_valid
+  end
+
+  it 'is not valid with blank attributes' do
+    task = Task.new
+    expect(task).to_not be_valid
+  end
+
+  it 'is not valid with invalid status' do
+    task = Task.new(
+      nome: 'Invalid Status Task',
+      descrição: 'Task with invalid status',
+      status: 'invalid_status'
+    )
+    expect(task).to_not be_valid
+  end
 end
