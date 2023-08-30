8f240bebc285d841e0f5535cb03dba47345ec030
tlamen <bernardolobo3@gmail.com>
2023-08-30 10:33:47 -0300

    :heavy_plus_sign: install switchUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,36 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//    "app/**/*.html.erb",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+import { colors } from '@switchdreams/ui'
+import switchUiPlugin from '@switchdreams/ui/dist/tailwind.config'
+
 module.exports = {
   content: [
-   "app/**/*.html.erb",
+    './node_modules/@switchdreams/ui/dist/**/*.js',
+    //...
   ],
   theme: {
-    extend: {},
+    extend: {
+      curvature: {
+        md: 0, // Configura a curvatura dos componentes
+        full: 0,
+      },
+      colors: colors, // Cores de 0 25 to 950 (altere as cores primaria e secundárias para os projetos)
+      fontFamily: {
+        default: ['Poppins', 'sans-serif'], //.. Opcional
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    switchUiPlugin(),
+  ],
+
 }
