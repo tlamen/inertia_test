ec9000f6abc024661b7ac8d1b28b344b93bc5746
tlamen <bernardolobo3@gmail.com>
2023-08-30 09:49:18 -0300

    :sparkles: valid model for tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+    validates :title, length: { minimum: 1 }, uniqueness: true
+    validates :description, length: { minimum: 1 }
+    validates :status, inclusion: ["pending", "in_progress", "completed", "suspended"]
 end
