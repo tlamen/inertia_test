a440884dd5a90148b8c651f5b0b45f811f6370e8
tlamen <bernardolobo3@gmail.com>
2023-08-31 17:56:55 -0300

    :fire: Remove GET code

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -3,35 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :request do
-  describe 'GET #index' do
-    it 'returns a list of all tasks' do
-      task1 = Task.create(title: 'Task 1', description: 'Description 1', status: 'pending')
-      task2 = Task.create(title: 'Task 2', description: 'Description 2', status: 'completed')
-
-      get :index
-
-      expect(response).to have_http_status(:ok)
-      parsed_response = JSON.parse(response.body)
-
-      expect(parsed_response.length).to eq(2)
-      expect(parsed_response[0]['title']).to eq(task1.title)
-      expect(parsed_response[1]['title']).to eq(task2.title)
-    end
-  end
-
-  describe 'GET #show' do
-    it 'returns a task by id' do
-      task = Task.create(title: 'Sample Task', description: 'Description', status: 'pending')
-      get :show, params: { id: task.id }
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'does not return a task for non-existent ID' do
-      get :show, params: { id: 999 }
-      expect(response).to have_http_status(:not_found)
-    end
-  end
-
   describe 'POST #create' do
     it 'creates a new task' do
       post :create, params: { task: { title: 'New Task', description: 'Description', status: 'pending' } }
