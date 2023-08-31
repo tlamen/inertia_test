22d5d1e4430b5264726e75903bbc08f3c16cf7d6
tlamen <bernardolobo3@gmail.com>
2023-08-31 19:40:11 -0300

    :construction: controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,22 @@ class TasksController < ApplicationController
       tasks: tasks
     }, status: :ok
   end
+
+  def create
+    Task.create!(task_params)
+    redirect_to tasks_path
+  rescue StandardError => e
+    redirect_to new_task_path, inertia: { errors: e }
+  end
+
+
+  private
+
+  def params
+    task_params.require(:task).permit(
+      'title',
+      'description',
+      'status'
+    )
+  end
 end
