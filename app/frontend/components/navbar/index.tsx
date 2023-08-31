2e3a9e47d73918f8fedc6b0bdc1047a33f9c9b30
tlamen <bernardolobo3@gmail.com>
2023-08-31 19:22:12 -0300

    :sparkles: :construction: start componentization

---
+++ app/frontend/components/navbar/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="rounded 50 h-11">nav</nav>
+    )
+}
+
+export default Navbar;
--- app/frontend/pages/home/index.tsx
+++ app/frontend/pages/home/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import Navbar from "../../components/navbar";
 
 const Index = (tasks) => {
     tasks.tasks.map((t) => {console.log(t.title)})
     // console.log(tasks.tasks[0].title)
     return (
-        <>
-            <h1>Hello World</h1>
+        <div className="flex-column">
+            <Navbar />
+            <h1 className="text-3xl font-bold underline">Hello World</h1>
             {tasks.tasks.map((t) => t.title)}
-        </>
+        </div>
     )
 }
 
